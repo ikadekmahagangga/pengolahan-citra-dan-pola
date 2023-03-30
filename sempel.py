5313ff14bcff516f5754c6cf1e15a3157c9284f6
I KADEK MAHA GANGGA <102144626+ikadekmahagangga@users.noreply.github.com>
2023-03-30 05:44:49 +0000

    3/30/2023

--- sempel.py
+++ sempel.py
@@ -1,7 +1,7 @@
 a = int(input("masukan sebuah angka= "))
-if (a>5):
-    print("nilai a lebih besar dari 5")
-elif (a<5):
-    print("nilai a lebih kecil dari 5")
+if  (a>10):
+    print("nilai a lebih besar dari 10")
+elif (a<10):
+    print("nilai a lebih kecil dari 10")
 else:
-    print("nilai a adalah 5")
+    print("nilai a adalah 10")
---
+++ sempel2.py
@@ -0,0 +1,2 @@
+bilangan = int(10)
+
